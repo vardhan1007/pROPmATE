93d716ffa709080ad373c0a1baf471e8c832a58f
vardhan1007 <svardhan1007@gmail.com>
2026-01-20 22:28:54 +0530

    fixed final next js 16 auth type compatibility

--- next.config.ts
+++ next.config.ts
@@ -2,19 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  reactCompiler: true,
-};
-
-export default nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
   typescript: {
-    // !! WARN !!
-    // This allows the build to finish even if there are type errors.
+    // This allows the build to finish even with type errors
     ignoreBuildErrors: true,
   },
-  // If you also want to skip ESLint checks to speed up the build:
   eslint: {
+    // This skips ESLint checks during build to save time
     ignoreDuringBuilds: true,
   },
 };
